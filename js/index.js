8b8e45da1f8eb2e7008e1329ecf862d9e0003305
IsmailHusseinCR <i.hussein08@gmail.com>
2019-01-09 23:01:22 +0100

    alert message

--- js/index.js
+++ js/index.js
@@ -1,34 +1,6 @@
-let mainNav = document.getElementById('js-menu');
-let navBarToggle = document.getElementById('js-navbar-toggle');
-
-navBarToggle.addEventListener('click', function () {
-    
-    mainNav.classList.toggle('active');
-});
-
-let slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1} 
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none"; 
-    }
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
-  slides[slideIndex-1].style.display = "block"; 
-  dots[slideIndex-1].className += " active";
-}
+function myFunction() {
+ document.getElementById("btn").addEventListener('click', () =>{
+   alert("Helaas is het niet mogelijk om de applicatie te downloaden op dit moment. Er is wel een link naar de clickable prototype beschikbaar");
+ })
+};
+myFunction();
